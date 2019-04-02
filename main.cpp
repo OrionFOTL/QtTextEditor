a257d110f2369a72b7f54058aae84128b2eac268
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-04-02 19:36:17 +0200

    Updated metadata

--- main.cpp
+++ main.cpp
@@ -7,5 +7,12 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
+    a.setApplicationName("Text Editor");
+    a.setApplicationVersion("0.1");
+    a.setOrganizationName("OrionSoft");
+    a.setOrganizationDomain("example.com");
+
+    a.setWindowIcon(QIcon(":/icons/new.png"));
+
     return a.exec();
 }
